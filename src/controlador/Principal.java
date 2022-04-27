587dc55ff46ec3404ac52a7b85c301bb7aa65c7a
mateomalagon <mateo.malagon.fp@iescampanillas.com>
2022-04-27 14:07:09 +0200

    Quinto commit esta vez con push

--- src/controlador/Principal.java
+++ src/controlador/Principal.java
@@ -8,13 +8,15 @@ package controlador;
  *
  */
 public class Principal {
-	String nombre = "Mateo";
+	
+	static String nombre = "Mateo";
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo");
+		System.out.println(nombre);
 	}
 
 }
