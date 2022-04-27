12d51dfd824cef1e08d3da76dca0283d8054464e
mateomalagon <mateo.malagon.fp@iescampanillas.com>
2022-04-27 13:44:56 +0200

    Tercer commit con una clase nueva en el paquete de código fuente

---
+++ src/controlador/Clase1.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package controlador;
+
+/**
+ * @author Usuario
+ *
+ */
+public class Clase1 {
+	private int dato1;
+	private int dato2;
+	/**
+	 * @return the dato1
+	 */
+	public int getDato1() {
+		return dato1;
+	}
+	/**
+	 * @param dato1 the dato1 to set
+	 */
+	public void setDato1(int dato1) {
+		this.dato1 = dato1;
+	}
+	/**
+	 * @return the dato2
+	 */
+	public int getDato2() {
+		return dato2;
+	}
+	/**
+	 * @param dato2 the dato2 to set
+	 */
+	public void setDato2(int dato2) {
+		this.dato2 = dato2;
+	}
+	/**
+	 * @param dato1
+	 * @param dato2
+	 */
+	public Clase1(int dato1, int dato2) {
+		super();
+		this.dato1 = dato1;
+		this.dato2 = dato2;
+	}
+	
+	
+}
