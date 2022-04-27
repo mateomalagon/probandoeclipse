b17ced2e22a44a8463c2a4c5e1346b29c632ce42
mateomalagon <mateo.malagon.fp@iescampanillas.com>
2022-04-27 13:35:35 +0200

    Primer commit con main vacío

---
+++ src/controlador/Principal.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package controlador;
+
+/**
+ * @author Usuario
+ *
+ */
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
